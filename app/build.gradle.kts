3ffc964d7b04288ca025f2b7aa7113552cc5ea51
Sevan <sevan.quesnot@gmail.com>
2026-05-11 00:04:33 +0200

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "1.5.2"
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
